6a5cd2e07225efbae8e700897f40103526e48b30
Vaakesan Sundrelingam <Vaakesan.Sundrelingam@unityhealth.to>
2022-03-22 16:38:34 -0400

    write header only once

--- pyDeid.py
+++ pyDeid.py
@@ -31,64 +31,83 @@ def pyDeid(original_file, new_file, phi_output_file, note_varname, encounter_id_
         notes = 0
         start_time = time.time()
 
-    writer = csv.DictWriter(open(new_file, 'w'), fieldnames=reader.fieldnames, lineterminator='\n')
-    writer.writeheader()
+    elif mode == "performance":
+        # write header
+        with open(phi_output_file, 'w', newline='') as o:
+            writer = csv.writer(o)
+            writer.writerow(
+                ['encounter_id', 'note_id', 'phi_start', 'phi_end', 'phi', 'surrogate_start', 'surrogate_end', 'surrogate', 'types']
+                )
 
-    for row in reader:
-        
-        original_note = row[note_varname]
+    with open(new_file, 'a') as f:
+        writer = csv.DictWriter(f, fieldnames=reader.fieldnames, lineterminator='\n')
+        writer.writeheader()
 
-        phi = name_first_pass(original_note)
-        find_phi(original_note, phi)
-        prune_phi(original_note, phi)
-        surrogates, new_note = replace_phi(original_note, phi, return_surrogates=True)
+        for row in reader:
+            
+            original_note = row[note_varname]
 
-        row[note_varname] = new_note
-        writer.writerow(row)
+            phi = name_first_pass(original_note)
+            find_phi(original_note, phi)
+            prune_phi(original_note, phi)
+            surrogates, new_note = replace_phi(original_note, phi, return_surrogates=True)
+
+            row[note_varname] = new_note
+            writer.writerow(row)
+
+            if mode == 'diagnostic':
+                chars += len(original_note)
+                notes += 1
+
+                key = row[encounter_id_varname]
+                if note_id_varname is not None:
+                    value = {row[note_id_varname]: surrogates}
+                else:
+                    value = {1: surrogates}
+
+                phi_output.setdefault(key, value).update(value)
+
+            elif mode == "performance":
+
+                phi_output = pd.DataFrame(surrogates)
+
+                if note_id_varname is not None:
+                    phi_output.insert(0, 'note_id', row[note_id_varname])
+                else:
+                    phi_output.insert(0, 'note_id', 1)
+
+                phi_output.insert(0, 'encounter_id', row[encounter_id_varname])
+
+                phi_output.to_csv(phi_output_file, mode = 'a', index = False, header = False)
 
         if mode == 'diagnostic':
-            chars += len(original_note)
-            notes += 1
-
-            key = row[encounter_id_varname]
-            if note_id_varname is not None:
-                value = {row[note_id_varname]: surrogates}
-            else:
-                value = {1: surrogates}
-
-            phi_output.setdefault(key, value).update(value)
-
-        elif mode == "performance":
-
-            phi_output = pd.DataFrame(surrogates)
-
-            if note_id_varname is not None:
-                phi_output.insert(0, 'note_id', row[note_id_varname])
-            else:
-                phi_output.insert(0, 'note_id', 1)
-
-            phi_output.insert(0, 'encounter_id', row[encounter_id_varname])
-
-            phi_output.to_csv(phi_output_file, mode = 'a', index = False)
-
-    if mode == 'diagnostic':
-        total_time = time.time() - start_time
-        print(
-            f"""Diagnostics:
-              - chars/s = {chars/total_time}
-              - s/note = {total_time/notes}"""
-            )
-        json.dump(phi_output, open(phi_output_file, 'w'), indent=4)
+            total_time = time.time() - start_time
+            print(
+                f"""Diagnostics:
+                - chars/s = {chars/total_time}
+                - s/note = {total_time/notes}"""
+                )
+            json.dump(phi_output, open(phi_output_file, 'w'), indent=4)
 
 if __name__ == "__main__":
-    
     pyDeid(
         'R:/GEMINI/De-identification Software/v2.0/Admission Notes Gold Standard/Validation of SMH Admission Notes/1/PyDeid/3239_AdmissionNotes_Apr2010_Oct2017.random700.csv', 
-        'R:/GEMINI/De-identification Software/v2.0/Admission Notes Gold Standard/Validation of SMH Admission Notes/1/PyDeid/3239_AdmissionNotes_Apr2010_Oct2017.random700_deid.csv', 
-        'R:/GEMINI/De-identification Software/v2.0/Admission Notes Gold Standard/Validation of SMH Admission Notes/1/PyDeid/3239_AdmissionNotes_Apr2010_Oct2017.random700_phi.csv', 
+        'R:/GEMINI/De-identification Software/v2.0/Admission Notes Gold Standard/Temp_test_files for SMH Admission Notes/3239_AdmissionNotes_Apr2010_Oct2017.random700_deid.csv', 
+        'R:/GEMINI/De-identification Software/v2.0/Admission Notes Gold Standard/Temp_test_files for SMH Admission Notes/3239_AdmissionNotes_Apr2010_Oct2017.random700_phi.csv', 
         'Value', 
         'genc_id', 
         'Encounter',
         mode = 'performance'
         )
+    """
+    pyDeid(
+        'test.csv', 
+        'test_deid.csv', 
+        'test_phi.csv', 
+        'note_text', 
+        'genc_id', 
+        'note_id',
+        mode = 'performance'
+        )
+    """
 
