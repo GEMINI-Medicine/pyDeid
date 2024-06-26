d3d19469307ec38fbb487a564c271c3357367d27
kairuie <kairui.personal@gmail.com>
2024-06-26 15:42:03 -0400

    Fixed replacement issue with MLL feature, update tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -10,69 +10,69 @@ filepath = os.path.dirname(os.path.realpath(__file__))
 #       )
 
 # @unittest.skip("Skipping default arguments test")
-# class TestCorrectOutputDefaultCsv(unittest.TestCase):
-#    # start_time = time.time()
+class TestCorrectOutputDefaultCsv(unittest.TestCase):
+   # start_time = time.time()
 
+   pyDeid(original_file = filepath + '\\test.csv', 
+      note_varname = 'note_text', 
+      encounter_id_varname = 'genc_id'
+      )
+   
+   # pyDeid(original_file = filepath + '\\tests_advanced\\100_words.csv', 
+   #    note_varname = 'note_text', 
+   #    encounter_id_varname = 'genc_id',
+   #    )
+   
+   # end_time = time.time()
+   
+   # print("Elapsed runtime: " + end_time-start_time)
+   def setUp(self):
+      print("Testing", self._testMethodName)
+   
+   def test_first_name_index(self):
+      with open(filepath + '\\test__PHI.csv', 'r') as csvfile:
+         csv_reader = csv.reader(csvfile)
+         next(csv_reader)
+         self.assertEqual(next(csv_reader)[3], 'Justin')
+   
+   def test_last_name_index(self):
+      with open(filepath + '\\test__PHI.csv', 'r') as csvfile:
+         csv_reader = csv.reader(csvfile)
+         next(csv_reader)
+         next(csv_reader)
+         self.assertEqual(next(csv_reader)[3], 'Bieber')
+
+   def test_correct_note_output(self):
+      with open(filepath + '\\test__DE-IDENTIFIED.csv', 'r') as csvfile:
+         csv_reader = csv.reader(csvfile)
+         next(csv_reader)
+         note = next(csv_reader)[2]
+         with open(filepath + '\\test__PHI.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            next(csv_reader)
+            next(csv_reader)
+            surrogate_name = next(csv_reader)[6]
+         note_end_name = note.find(surrogate_name) + len(surrogate_name)
+         self.assertNotEqual(note[:note_end_name], 'Justin Bieber')
+         self.assertEqual(note[note_end_name:note_end_name+len(' was born on ')], ' was born on ')
+
+# @unittest.skip("Skipping MLL only argument test")
+# class TestCorrectOutputMllOnlyCsv(unittest.TestCase):
 #    # pyDeid(original_file = filepath + '\\test.csv', 
 #    #    note_varname = 'note_text', 
-#    #    encounter_id_varname = 'genc_id'
+#    #    encounter_id_varname = 'genc_id',
+#    #    regex_find = False,
+#    #    regex_replace = False,
+#    #    mll_file = filepath + '\\mll_test.csv'
 #    #    )
    
 #    pyDeid(original_file = filepath + '\\tests_advanced\\100_words.csv', 
 #       note_varname = 'note_text', 
 #       encounter_id_varname = 'genc_id',
+#       regex_find = False,
+#       regex_replace = False,
+#       mll_file = filepath + '\\tests_advanced\\mll_100_test.csv'
 #       )
-   
-   # end_time = time.time()
-   
-   # print("Elapsed runtime: " + end_time-start_time)
-#    def setUp(self):
-#       print("Testing", self._testMethodName)
-   
-#    def test_first_name_index(self):
-#       with open(filepath + '\\test__PHI.csv', 'r') as csvfile:
-#          csv_reader = csv.reader(csvfile)
-#          next(csv_reader)
-#          self.assertEqual(next(csv_reader)[3], 'Justin')
-   
-#    def test_last_name_index(self):
-#       with open(filepath + '\\test__PHI.csv', 'r') as csvfile:
-#          csv_reader = csv.reader(csvfile)
-#          next(csv_reader)
-#          next(csv_reader)
-#          self.assertEqual(next(csv_reader)[3], 'Bieber')
-
-#    def test_correct_note_output(self):
-#       with open(filepath + '\\test__DE-IDENTIFIED.csv', 'r') as csvfile:
-#          csv_reader = csv.reader(csvfile)
-#          next(csv_reader)
-#          note = next(csv_reader)[2]
-#          with open(filepath + '\\test__PHI.csv', 'r') as csvfile:
-#             csv_reader = csv.reader(csvfile)
-#             next(csv_reader)
-#             next(csv_reader)
-#             surrogate_name = next(csv_reader)[6]
-#          note_end_name = note.find(surrogate_name) + len(surrogate_name)
-#          self.assertNotEqual(note[:note_end_name], 'Justin Bieber')
-#          self.assertEqual(note[note_end_name:note_end_name+len(' was born on ')], ' was born on ')
-
-# @unittest.skip("Skipping MLL only argument test")
-class TestCorrectOutputMllOnlyCsv(unittest.TestCase):
-   # pyDeid(original_file = filepath + '\\test.csv', 
-   #    note_varname = 'note_text', 
-   #    encounter_id_varname = 'genc_id',
-   #    regex_find = False,
-   #    regex_replace = False,
-   #    mll_file = filepath + '\\mll_test.csv'
-   #    )
-   
-   pyDeid(original_file = filepath + '\\tests_advanced\\100_words.csv', 
-      note_varname = 'note_text', 
-      encounter_id_varname = 'genc_id',
-      regex_find = False,
-      regex_replace = False,
-      mll_file = filepath + '\\tests_advanced\\mll_100_test.csv'
-      )
 
 
 #    def setUp(self):
