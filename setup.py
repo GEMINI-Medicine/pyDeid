611c03ca659d54d7d8e417395014c8a0b9ac9427
weihanbillliu <147636115+weihanbillliu@users.noreply.github.com>
2025-09-23 16:57:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   packages=['pyDeid', 'pyDeid.phi_types', 'pyDeid.process_note'],
   package_dir={'pyDeid': 'src/pyDeid'},
   package_data={'pyDeid': ['wordlists/*.txt']},
-  version='0.0.3',
+  version='1.0.0',
   license='MIT',
   description='Replaces personal health information in free text.',
   long_description=long_description,
